965941c6074c589385ab7c0df3b1cc786cd5f3ee
JavaBloxx <joseph.davies2@outlook.com>
2020-05-31 23:11:43 +1000

    Initial commit

---
+++ src/main/java/com/sid/projects/torn/exceptions/DbcClientException.java
@@ -0,0 +1,8 @@
+package com.sid.projects.torn.exceptions;
+
+public class DbcClientException extends Exception
+{
+    public DbcClientException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/sid/projects/torn/exceptions/UnsolvedCaptcha.java
@@ -0,0 +1,8 @@
+package com.sid.projects.torn.exceptions;
+
+public class UnsolvedCaptcha extends Exception
+{
+    public UnsolvedCaptcha(String message) {
+        super(message);
+    }
+}
